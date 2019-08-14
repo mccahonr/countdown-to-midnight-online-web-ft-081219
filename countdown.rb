250b232a097bc5b4026ffc3122e4fbcfe0993f14
Ryan McCahon <mccahonr@yahoo.com>
2019-08-14 18:39:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown(counter)
+  phrase = "HAPPY NEW YEAR!"
+  counter = 10
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  phrase
+end
+
+def countdown_with_sleep(number)
+    sleep(5)
 end
